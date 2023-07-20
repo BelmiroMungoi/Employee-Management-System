b0a67dc8f5809b2f319dddbc47610302ab69c7ac
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-20 20:04:41 +0200

    the login service

---
+++ src/app/service/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AppConstants } from '../app-constants';
+import { Observable } from 'rxjs'
+import { LoginRequestPayload } from '../auth/login/login-request.payload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(userLogin: LoginRequestPayload): Observable<any>{
+    return this.http.post(AppConstants.baseLogin, userLogin, {responseType: 'text'})         
+  } 
+}
