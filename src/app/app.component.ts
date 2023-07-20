ed6e869d4467539477071ef265f5d5988c3caae1
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-20 19:56:17 +0200

    verifying if the user is authenticated to see the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Employee-Management-System';
+  isMenuRequired = false;
+
+  constructor(private router: Router) {
+    let currentUrl = this.router.url;
+    if (currentUrl == '/login' || currentUrl == '/register') {
+      this.isMenuRequired = false;
+    } else {
+      this.isMenuRequired = true;
+    }
+  }
+  public hideMenu() {
+    if (localStorage.getItem('access_token') != null &&
+      localStorage.getItem('access_token')?.toString().trim() != null) {
+        return false;
+    } else {
+      return true;
+    }
+  }
+
 }
