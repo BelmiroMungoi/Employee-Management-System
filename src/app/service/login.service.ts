a4a5cda2ca4eaf424bd00e5762a30c75cd6e281c
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-21 23:24:35 +0200

    removing the response type from login

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { AppConstants } from '../app-constants';
 import { Observable } from 'rxjs'
 import { LoginRequestPayload } from '../model/login-request.payload';
@@ -10,9 +9,9 @@ import { LoginRequestPayload } from '../model/login-request.payload';
 })
 export class LoginService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) { }
 
   login(userLogin: LoginRequestPayload): Observable<any>{
-    return this.http.post(AppConstants.baseLogin, userLogin, {responseType: 'text'})         
+    return this.http.post(AppConstants.baseLogin, userLogin);         
   } 
 }
