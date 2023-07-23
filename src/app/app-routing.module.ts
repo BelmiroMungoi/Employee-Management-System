b5b393dc30a98fd4bb78575b7f972ceb2d9bbb8d
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-23 20:32:39 +0200

    adding a new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { DeparmentComponent } from './component/deparment/deparment.component';
+import { MissionComponent } from './component/mission/mission.component';
+import { EmployeeAddComponent } from './component/employee-add/employee-add.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent }, 
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'employee', component: EmployeeComponent},
+  { path: 'employeeAdd', component: EmployeeAddComponent},
+  { path: 'department', component: DeparmentComponent},
+  { path: 'mission', component: MissionComponent}
 ];
 
 @NgModule({
