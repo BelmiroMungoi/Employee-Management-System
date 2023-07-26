588083ebba3fb15c24d63e731e4a6df834e02eab
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-26 18:02:53 +0200

    saving and getting all employees

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EmployeeRequestPayload } from '../model/employee-request.payload';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../app-constants';
+import { EmployeeResponsePayload } from '../model/employee-response.payload';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createEmployee(employee: EmployeeRequestPayload): Observable<any> {
+    return this.http.post(AppConstants.baseServer + "/employee/", employee);
+  }
+
+  getAllEmployee(): Observable<EmployeeResponsePayload[]> {
+    return this.http.get<EmployeeResponsePayload[]>(AppConstants.baseServer + "/employee/");
+  }
 }
