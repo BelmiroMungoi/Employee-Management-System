f0cdd84b7507b8d78cefb2b5dd11db9587342a92
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-26 18:01:18 +0200

    getting all the departments

--- src/app/service/department.service.ts
+++ src/app/service/department.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Department } from '../model/department.payload';
+import { AppConstants } from '../app-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(AppConstants.baseServer + "/department/")
+  }
 }
