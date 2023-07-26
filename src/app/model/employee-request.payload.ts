46d52c1cd46f1d1f3fec64d0f3e98e823dcfa839
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-26 18:00:45 +0200

    the employee request payload

---
+++ src/app/model/employee-request.payload.ts
@@ -0,0 +1,12 @@
+export class EmployeeRequestPayload {
+
+    firstname!: string;
+    lastname!: string;
+    email!: string;
+    birthdate!: string;
+    department!: string;
+    street!: string;
+    houseNumber!: string;
+    zipCode!: string;
+    
+}
