38eac2dea1eaba760fd927c7ead19c559304e78e
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-26 18:03:21 +0200

    the guardian

---
+++ src/app/service/guardian.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardianGuard implements CanActivate {
+
+  constructor(private userService: LoginService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.userService.isAuthenticated();
+  }
+
+
+} 
