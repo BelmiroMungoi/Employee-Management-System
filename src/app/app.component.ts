27a4b0de60c58b80d006e21a1e4194c362d1e6c7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-26 17:57:20 +0200

    add the method to toggle the sideBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,13 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Employee-Management-System';
 
   constructor() {}
+  ngOnInit(): void {
+    this.toggleSideBar();
+  }
 
   public hideMenu() {
     if (localStorage.getItem('access_token') != null &&
@@ -17,6 +20,34 @@ export class AppComponent {
         return false;
     } else {
       return true;
+    }
+  }
+
+   public toggleSideBar() {    
+    const select = (el: any, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type: any, el: any, listener: any, all = false) => {
+      if (all) {
+        select(el, all).forEach((e: any) => e.addEventListener(type, listener))
+      } else {
+        select(el, all).addEventListener(type, listener)
+      }
+    }
+
+    if (select('.toggle-sidebar-btn')) {
+      on('click', '.toggle-sidebar-btn', function(e: any) {
+        select('body').classList.toggle('toggle-sidebar')
+      })
     }
   }
 
