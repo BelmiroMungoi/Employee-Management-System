4413e4f42de67c149ae6641e75f8b2173157a17d
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-28 20:12:28 +0200

    searching the employees by name

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,8 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { first } from 'rxjs';
 import { DataTable } from 'simple-datatables';
 import { EmployeeResponsePayload } from 'src/app/model/employee-response.payload';
+import { SearchRequest } from 'src/app/model/search-request.payload';
 import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
@@ -10,27 +13,62 @@ import { EmployeeService } from 'src/app/service/employee.service';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit{
- 
-  constructor(private employeeService: EmployeeService, private toastr: ToastrService) {}
+export class EmployeeComponent implements OnInit {
 
   employees!: EmployeeResponsePayload[];
+  searchForm!: FormGroup;
+  searchRequest!: SearchRequest;
+  name!: string;
   total!: number;
+
+  constructor(private employeeService: EmployeeService, private toastr: ToastrService) {
+    this.searchRequest = {
+      firstname: '',
+      lastname: '',
+      email: ''
+    }
+  }
 
   ngOnInit(): void {
     this.getAllEmployees();
+    this.searchForm = new FormGroup({
+      firstname: new FormControl('')
+    })
   }
 
   public getAllEmployees() {
     this.employeeService.getAllEmployee().subscribe(
       (response: EmployeeResponsePayload[]) => {
         this.employees = response;
-      }, 
+      },
       (error: HttpErrorResponse) => {
-        this.toastr.error("Aconteceu um erro ao carregar a lista de funcionários!")
+        this.toastr.error("Ocorreu um erro ao carregar a lista de funcionários!")
         console.info(error.message);
       }
     );
+  }
+
+  public searchEmployeeByFirstname() {
+    this.name = this.searchForm.get('firstname')?.value;
+    if (this.name == null || this.name == '') {
+      this.getAllEmployees();
+
+    } else {
+      this.employeeService.getEmployeeByFirstname(this.name).subscribe(response => {
+        this.employees = response;
+        console.info(response);
+      }, error => {
+        this.toastr.error("Ocorreu um erro ao pesquisar funcionário!");
+        console.error(error.message);
+      })
+    }
+  }
+
+  public deleteEmployee(id: Number, index: any) {
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      this.employees.splice(index, 1);
+      this.toastr.success(data);
+    })
   }
 
 }
