e9580031bb6abe524b3500edc8dee812d658ce26
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-04 22:12:24 +0200

    getting all the missions

--- src/app/component/mission/mission.component.ts
+++ src/app/component/mission/mission.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MissionResponsePayload } from 'src/app/model/mission-response.payload';
+import { MissionService } from 'src/app/service/mission.service';
 
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.component.html',
   styleUrls: ['./mission.component.css']
 })
-export class MissionComponent {
+export class MissionComponent implements OnInit {
+
+  missionResponse!: MissionResponsePayload[];
+
+  constructor(private missionService: MissionService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.getAllMission();
+  }
+
+  getAllMission() {
+    this.missionService.getAllMission().subscribe(response => {
+      this.missionResponse = response;
+      console.info(response);
+    }, error => {
+      this.toastr.error("Ocorreu um erro ao carregar a lista de projectos!");
+      console.error(error);
+    })
+  }
+
 
 }
