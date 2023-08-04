02b1903e9259973bbc52f56c1d47a4a1ffeb887c
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-04 22:14:11 +0200

    creating and getting all the missions

--- src/app/service/mission.service.ts
+++ src/app/service/mission.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MissionRequestPayload } from '../model/mission-request.payload';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../app-constants';
+import { MissionResponsePayload } from '../model/mission-response.payload';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MissionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createMission(mission: MissionRequestPayload): Observable<any> {
+    return this.http.post(AppConstants.baseServer + "/mission/", mission);
+  }
+
+  getAllMission(): Observable<MissionResponsePayload[]> {
+    return this.http.get<MissionResponsePayload[]>(AppConstants.baseServer + "/mission/");
+  }
 }
