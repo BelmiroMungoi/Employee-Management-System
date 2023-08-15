54d736213a54c096aaaa553abda72ae9dfe177d2
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-15 23:04:37 +0200

    uploading and downloading the image

---
+++ src/app/service/image.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../app-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadImage(image: any): Observable<any> {
+    return this.http.post(AppConstants.baseServer + "/image/upload", image, { responseType: 'text', observe: 'response' });
+  }
+
+  uploadImageForEmployee(image: any, id: number): Observable<any> {
+    return this.http.post(AppConstants.baseServer + "/image/upload/" + id, image);
+  }
+
+  downloadImage(): Observable<any> {
+    return this.http.get<any>(AppConstants.baseServer + "/image/download/");
+  }
+}
