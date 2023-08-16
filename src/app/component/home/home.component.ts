78c18549e7e8a51306c970168754ebcdb60a5ace
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-16 12:52:31 +0200

    getting the users enabled quantity

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentService } from 'src/app/service/department.service';
 import { EmployeeService } from 'src/app/service/employee.service';
 import { MissionService } from 'src/app/service/mission.service';
+import { UserService} from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,10 @@ export class HomeComponent implements OnInit {
   employee!: number;
   department!: number;
   mission!: number;
+  user!: number;
 
   constructor(private employeeService: EmployeeService, private departmentService: DepartmentService,
-    private missionService: MissionService) { }
+    private missionService: MissionService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.getQuantities();
@@ -32,6 +34,10 @@ export class HomeComponent implements OnInit {
 
     this.missionService.getMissionQuantity().subscribe(response => {
       this.mission = response;
+    });
+
+    this.userService.getEnabledUsersQuantity().subscribe(response => {
+      this.user = response;
     });
   }
 }
