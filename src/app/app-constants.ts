1f958bc016d42b15c8316b71e54b7e447557074a
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-24 22:32:56 +0200

    passing the new server link

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
 
     public static get baseServer(): string {
-        return "http://localhost:8080/api/v1";
+        return "https://employee-management-bbm-78ef7c687792.herokuapp.com/api/v1";
     }
 
     public static get baseLogin(): string {
