267a230eafb47beef4853ae71b59802e1b2d488e
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-09-19 20:58:01 +0200

    add a refresh-token

--- src/app/service/header-interceptor.service.ts
+++ src/app/service/header-interceptor.service.ts
@@ -1,42 +1,95 @@
 import { Injectable, NgModule } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpResponse, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpResponse, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, filter, switchMap, take, tap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { LoginService } from './login.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HeaderInterceptorService implements HttpInterceptor{
+export class HeaderInterceptorService implements HttpInterceptor {
 
-  constructor(private toastr: ToastrService) { }
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  constructor(private toastr: ToastrService, private router: Router, private loginService: LoginService) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (localStorage.getItem('access_token') != null && localStorage.getItem('access_token')?.toString().trim() != null) {
-      const token = 'Bearer ' + localStorage.getItem('access_token');
-      const tokenRequest = req.clone({
-        headers: req.headers.set('Authorization', token)
-      });
+      var token = localStorage.getItem('access_token');
+      var tokenRequest = this.addTokenHeader(req, token);
 
-      return next.handle(tokenRequest).pipe(tap((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse && (event.status === 200 || event.status === 201)) {
-          //console.info("Sucesso na operacão")
+      return next.handle(tokenRequest).pipe(catchError((error: HttpErrorResponse) => {
+        if (error instanceof HttpErrorResponse && error.status === 403) {
+          console.log('Erro 403 teste')
+          this.handle403Error(req, next);
         }
-      }),
-        catchError(this.processError)
-      );
+
+        return throwError(error);
+      })); 
     } else {
       return next.handle(req).pipe(catchError(this.processError));
     }
   }
 
+  refreshToken() {
+    this.loginService.refreshToken(localStorage.getItem('refresh_token')).subscribe(data => {
+      localStorage.setItem("access_token", data.access_token);;
+    }, error => {
+      localStorage.clear();
+      this.router.navigate(['login']);
+    })
+  }
+
+  private handle403Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing == true) {
+      
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+
+      var token = localStorage.getItem('refresh_token');
+
+      if (token)
+        return this.loginService.refreshToken(token).pipe(
+          switchMap((token: any) => {
+            console.log('Passou por aqui')
+            this.isRefreshing = false;
+            localStorage.removeItem('access_token');
+            localStorage.setItem('access_token', token.access_token)
+            this.refreshTokenSubject.next(token.accessToken);
+
+            return next.handle(this.addTokenHeader(request, token.accessToken));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            localStorage.clear();
+            this.router.navigate(['login']);
+
+            return throwError(err);
+          })
+        );
+    }
+
+    return this.refreshTokenSubject.pipe(
+      filter(token => token !== null),
+      take(1),
+      switchMap((token) => next.handle(this.addTokenHeader(request, token)))
+    );
+  }
+
+  private addTokenHeader(req: HttpRequest<any>, token: any) {
+    return req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + token)
+    });
+  }
+
   processError(error: HttpErrorResponse) {
     let errorMessage = 'Erro';
-
     if (error.error instanceof ErrorEvent) {
       console.error(error.error);
-      errorMessage = 'Erro: ' + error.error.error; 
-    } else if (error.status == 403) {
-      errorMessage = 'Ocorreu um erro interno';
+      errorMessage = 'Erro: ' + error.error.error;
     } else {
       errorMessage = 'Código: ' + error.error.code + error.error.error;
     }
