a5f520181903d5ccd1f6a9e77ed78377572cacd7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-14 15:19:11 +0200

    adding the http request to change the password

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppConstants } from '../app-constants';
 import { RegisterRequestPayload } from '../model/register-request.payload';
+import { ChangePasswordRequest } from '../model/changePassword-request.payload';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,9 @@ export class UserService {
 
   updateUser(request: RegisterRequestPayload): Observable<any> {
     return this.http.put(AppConstants.baseServer + "/user/", request);
+  }
+
+  changePassword(request: ChangePasswordRequest): Observable<any> {
+    return this.http.put(AppConstants.baseServer + "/user/changePassword", request);
   }
 }
